a96ec6f5970600a1a080cc890ad252571ed3394d
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-11 05:17:21 +0200

    change logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test-app',
+    title: 'MovieApp',
     htmlAttrs: {
       lang: 'en'
     },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'http://localhost:3000/_nuxt/assets/images/movieHero.jpg' },
       {rel: 'stylesheet' , href: "https://fonts.googleapis.com/css?family=Tangerine"}
     ]
   },
